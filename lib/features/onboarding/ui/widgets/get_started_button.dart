eb87e990306d579355a6b9c3eef87cf525fa33d5
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-07-18 12:03:28 +0300

    add get started button in onboarding

---
+++ lib/features/onboarding/ui/widgets/get_started_button.dart
@@ -0,0 +1,36 @@
+import 'package:doc/core/helpers/routing_extension.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/routing/routes.dart';
+import '../../../../core/theming/app_colors.dart';
+import '../../../../core/theming/text_styles.dart';
+
+
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        context.pushNamed(Routes.loginScreen);
+      },
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(ColorManager.mainBlue),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        minimumSize: MaterialStateProperty.all(
+          const Size(double.infinity, 52),
+        ),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+      ),
+      child: Text(
+        'Get Started',
+        style: TextStyles.font16WhiteMedium,
+      ),
+    );
+  }
+}
