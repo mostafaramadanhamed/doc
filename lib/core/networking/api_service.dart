861cbcaaa1e9a53dc26404ee77ae1cc16ffef0e3
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-07-24 12:45:14 +0300

    edit login post method

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -1,6 +1,8 @@
 
 import 'package:dio/dio.dart';
 import 'package:doc/core/networking/api_constants.dart';
+import 'package:doc/features/login/data/models/login_request_body.dart';
+import 'package:doc/features/login/data/models/login_response.dart';
 import 'package:retrofit/http.dart';
 part 'api_service.g.dart';
 
@@ -9,7 +11,7 @@ abstract class ApiService{
   factory ApiService(Dio dio ,{String baseUrl})=_ApiService;
 
   @POST(ApiConstants.login)
-  Future<String>login(
-      @Body()Map<String,dynamic>body,
+  Future<LoginResponse>login(
+      @Body()LoginRequestBody loginRequestBody,
       );
 }
