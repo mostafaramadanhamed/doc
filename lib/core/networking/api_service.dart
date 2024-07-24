801d00a87201f1fedf5298c760957f03996f0428
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-07-24 11:35:11 +0300

    add auto generated api service

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -2,6 +2,8 @@
 import 'package:dio/dio.dart';
 import 'package:doc/core/networking/api_constants.dart';
 import 'package:retrofit/http.dart';
+part 'api_service.g.dart';
+
 @RestApi(baseUrl: ApiConstants.apiBaseUrl)
 abstract class ApiService{
   factory ApiService(Dio dio ,{String baseUrl})=_ApiService;
