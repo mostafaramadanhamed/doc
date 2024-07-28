f87352c21c858d98dcc28e42284b5e84442094b8
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-07-28 11:43:14 +0300

    add bloc provider in login screen in app router

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,7 +1,10 @@
+import 'package:doc/core/di/dependency_injection.dart';
 import 'package:doc/core/routing/routes.dart';
+import 'package:doc/features/login/logic/login_cubit.dart';
 import 'package:doc/features/login/ui/login_screen.dart';
 import 'package:doc/features/onboarding/ui/onboarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
@@ -15,7 +18,9 @@ class AppRouter {
         );
       case Routes.loginScreen:
         return MaterialPageRoute(
-          builder: (_) => const LoginScreen(),
+          builder: (_) => BlocProvider(
+              create: (context)=>getIt<LoginCubit>(),
+              child: const LoginScreen()),
         );
       case Routes.homeScreen:
         return MaterialPageRoute(
