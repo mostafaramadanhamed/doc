830c38c9f982e11831ffda08cd5e51c3d4e86923
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-07-30 10:40:58 +0300

    add signup method in api service

--- lib/core/networking/api_constants.dart
+++ lib/core/networking/api_constants.dart
@@ -1,6 +1,8 @@
 class ApiConstants{
   static const String apiBaseUrl='https://vcare.integration25.com/api/';
   static const String login='auth/login';
+  static const String signup = "auth/register";
+
 }
 
 class ApiErrors {
