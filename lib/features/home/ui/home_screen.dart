f8b636899d3f5de07ecc4b42e1354a1b89417bf1
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-08-16 20:10:20 +0300

    = refactor specializations and doctors bloc builder

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../core/helpers/spacing_extension.dart';
-import '../logic/home_cubit.dart';
-import '../logic/home_state.dart';
-import 'widgets/doctor_specialty_list_view.dart';
 import 'widgets/doctor_specialty_see_all.dart';
 import 'widgets/doctors_blue_screen.dart';
-import 'widgets/doctors_list_view.dart';
 import 'widgets/home_top_bar.dart';
+import 'widgets/specialization_and_doctors_bloc_builder.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -29,42 +25,12 @@ class HomeScreen extends StatelessWidget {
               verticalSpace(24),
               const DoctorSpecialtySeeAll(),
               verticalSpace(18),
-              BlocBuilder<HomeCubit, HomeState>(
-                buildWhen: (previous, current) =>
-                    current is SpecializationsSuccess ||
-                    current is SpecializationsError ||
-                    current is SpecializationsLoading,
-                builder: (context, state) {
-                  return state.maybeWhen(specializationsLoading: () {
-                    return const SizedBox(
-                      height: 100,
-                      child: CircularProgressIndicator(),
-                    );
-                  }, specializationsSuccess: (specializationResponseModel) {
-                    var specializationsList = specializationResponseModel.specializationDataList;
-                    return Column(
-                      children: [
-                        DoctorsSpecialityListView(
-                          specializationDataList: specializationsList??[],
-                        ),
-                        verticalSpace(8),
-                         DoctorsListView(doctorsList: specializationsList.??,),
-                      ],
-                    );
-                  }, specializationsError: (error) {
-                    return const SizedBox.shrink();
-                  }, orElse: () {
-                    return const SizedBox.shrink();
-                  });
-                },
-              ),
-              // const DoctorsSpecialityListView(),
-              // verticalSpace(8),
-              // const DoctorsListView(),
+              const SpecializationsAndDoctorsBlocBuilder(),
             ],
           ),
         ),
       ),
     );
   }
+
 }
--- lib/features/home/ui/widgets/doctors_list_view_item.dart
+++ lib/features/home/ui/widgets/doctors_list_view_item.dart
@@ -5,13 +5,13 @@ import '../../../../core/helpers/spacing_extension.dart';
 import '../../../../core/theming/text_styles.dart';
 import '../../data/models/specialization_response_model.dart';
 
-class DoctorListViewItem extends StatelessWidget {
-  const DoctorListViewItem({
+class DoctorsListViewItem extends StatelessWidget {
+  const DoctorsListViewItem({
     super.key,
-    required this.doctor,
+    required this.doctorsModel,
   });
 
-  final Doctors? doctor;
+  final Doctors? doctorsModel;
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +24,7 @@ class DoctorListViewItem extends StatelessWidget {
             child: Image.network(
               width: 110.w,
               height: 120.h,
-              doctor!.photo ?? 'https://via.placeholder.com/150',
+              doctorsModel!.photo ?? 'https://via.placeholder.com/150',
               fit: BoxFit.cover
             ),
           ),
@@ -34,18 +34,18 @@ class DoctorListViewItem extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                 doctor!.name ?? 'Doc Doc',
+                 doctorsModel!.name ?? 'Doc Doc',
                   style: TextStyles.font18DarkBlueBold,
                   overflow: TextOverflow.ellipsis,
                 ),
                 verticalSpace(5),
                 Text(
-             '${ doctor!.degree}| ${doctor!.phone}',
+             '${ doctorsModel!.degree}| ${doctorsModel!.phone}',
                   style: TextStyles.font12GrayMedium,
                 ),
                 verticalSpace(5),
                 Text(
-                doctor!.email??"Email",
+                doctorsModel!.email??"Email",
                   style: TextStyles.font12GrayMedium,
                 ),
               ],
