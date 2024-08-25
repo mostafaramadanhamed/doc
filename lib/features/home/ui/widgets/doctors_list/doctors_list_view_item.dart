416bf1de9ba85cb3b9e27ea76f898e361be00700
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-08-25 13:02:13 +0300

    add shimmer loading for doctor image

--- lib/features/home/ui/widgets/doctors_list/doctors_list_view_item.dart
+++ lib/features/home/ui/widgets/doctors_list/doctors_list_view_item.dart
@@ -1,8 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
 
 import '../../../../../core/helpers/spacing_extension.dart';
+import '../../../../../core/theming/app_colors.dart';
 import '../../../../../core/theming/text_styles.dart';
 import '../../../data/models/specialization_response_model.dart';
 
@@ -28,8 +30,21 @@ class DoctorsListViewItem extends StatelessWidget {
               imageUrl:
                   doctorsModel!.photo ?? 'https://via.placeholder.com/150',
               fit: BoxFit.cover,
-              progressIndicatorBuilder: (context, url, downloadProgress) =>
-                  CircularProgressIndicator(value: downloadProgress.progress),
+              progressIndicatorBuilder: (context, url, downloadProgress) {
+                return Shimmer.fromColors(
+                  baseColor: ColorsManager.lightGray,
+                  highlightColor: Colors.white,
+                  child: Container(
+                    width: 110.w,
+                    height: 120.h,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(12.0),
+                      color: Colors.white,
+                    ),
+                  ),
+                );
+              },
               errorWidget: (context, url, error) => const Icon(Icons.error),
             ),
           ),
